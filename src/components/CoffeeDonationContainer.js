f49076984fae674df52647d66c6db679fa38c404
Bobbylee Ingalls <34226701+ReesesSnickers@users.noreply.github.com>
2022-01-10 18:06:25 -0600

    Added Report Button and Corrected Copy Typos (#49)
    * reportabugbutton Applied report bug button in nav menu. Corrected minor copy typos. Applied minor updates for future donate feature
    
    * Corrected console warnings

--- src/components/CoffeeDonationContainer.js
+++ src/components/CoffeeDonationContainer.js
@@ -1,17 +1,54 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-// import { faMugHot } from '@fortawesome/free-solid-svg-icons';
-// import NavMenuIconButton from './buttons/NavMenuIconButton';
+import { faMugHot } from '@fortawesome/free-solid-svg-icons';
+import Colors from '../utility/constants/colors';
+
 /* DEV NOTE: Apply a donation tied to a payment gateway service */
-/* <NavMenuIconButton
-        href="#"
-        ariaLabel="Donate a coffee to Bobbylee Ingalls"
-        icon={faMugHot}
-        className="coffee"
-      /> */
+/* DEV NOTE: need to apply a infinate animation that helps guide users to donate */
 
 const CoffeeDonationContainer = () => {
   return (
-    <h5>Like what you see? Please donate and support my coffee addiction!</h5>
+    <section>
+      <div className="icon-wrapper">
+        <FontAwesomeIcon icon={faMugHot} />
+      </div>
+      <h5>
+        Like what you see? Please{' '}
+        {/* <a href="#" target="_blank" rel="noopener noreferrer"> */}
+        donate
+        {/* </a>{' '} */}
+        and support my coffee addiction!
+      </h5>
+      <style jsx>{`
+        h5 {
+          text-align: center;
+        }
+      `}</style>
+      <div className="icon-wrapper">
+        <FontAwesomeIcon icon={faMugHot} />
+      </div>
+      <style jsx>{`
+        a {
+          color: ${Colors.BRIGHT_GREEN};
+          text-decoration: none;
+        }
+        a:visited {
+          color: ${Colors.BRIGHT_GREEN};
+        }
+
+        section {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+        }
+        .icon-wrapper:first-of-type {
+          margin-right: 10px;
+        }
+        .icon-wrapper:last-of-type {
+          margin-left: 10px;
+        }
+      `}</style>
+    </section>
   );
 };
 
---
+++ src/components/NavMenuEndContainer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Feature from '../utility/featureConfig';
+import Colors from '../utility/constants/colors';
+import BugReportNavMenuLink from './BugReportNavMenuLink';
+import CoffeeDonationContainer from './CoffeeDonationContainer';
+
+const NavMenuEndContainer = ({ closeMenu }) => {
+  return (
+    <section className="end-container">
+      <ul>
+        <li onClick={closeMenu}>
+          <BugReportNavMenuLink />
+        </li>
+      </ul>
+      {Feature.showDevCoffeeSupport ? <CoffeeDonationContainer /> : null}
+      <style jsx>{`
+        section {
+          padding: 10px;
+        }
+        .end-container li {
+          padding: 10px;
+          height: 60px;
+          border-radius: 3px;
+          overflow: hidden;
+        }
+        .end-container li:hover {
+          box-shadow: 0px 0px 3px 5px ${Colors.BRIGHT_GREEN};
+          -moz-transition: all 0.2s ease-in;
+          -o-transition: all 0.2s ease-in;
+          -webkit-transition: all 0.2s ease-in;
+          transition: all 0.2s ease-in;
+        }
+      `}</style>
+    </section>
+  );
+};
+
+export default NavMenuEndContainer;
